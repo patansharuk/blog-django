4ddaf1d9192e8c3c719d5b0cfff4df4324cc94b5
sharukhan <sharukhan@webkorps.com>
2024-04-24 23:03:44 +0530

    added post update edit delete show feature and its templates

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, re_path
-from .views import Blogs, SUDBlog, Comments, catch_all_view
+from .views import Blogs, RevokeBlog, Comments, catch_all_view
 
 urlpatterns = [
     path("",Blogs.as_view(),name="blogs_path"),
-    # path("<int:blog_id>/edit",Blogs.edit,name="edit_blog_path"),
-    # path("<int:blog_id>",Blogs.as_view(),name="blogs_path"),
+    path("create",RevokeBlog.create_blog,name="create_blog_path"),
+    path("<int:blog_id>/show",RevokeBlog.show_blog,name="show_blog_path"),
+    path("<int:blog_id>/edit",RevokeBlog.edit_blog,name="edit_blog_path"),
+    path("<int:blog_id>/update",RevokeBlog.update_blog,name="update_blog_path"),
+    path("<int:blog_id>/destroy",RevokeBlog.delete_blog,name="delete_blog_path"),
 
     # path("<int:blog_id>/comments",Comments.index,name="blog_comments_path"),
     # path("<int:blog_id>/comment/create",Comments.create,name="create_comment"),
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, QueryDict
 from django.views.generic import TemplateView
 from django.template import loader
 from blog.models import Blog
+from django.contrib import messages
 
 class Blogs(TemplateView):
     template_name = 'home.html'
 
     def get(self, request):
+        # fetch the blogs
         blogs = Blog.objects.all()
-        return render(request, self.template_name, {'blogs': blogs, 'message': f'found {len(blogs)} blogs'})
+        message = f'found {len(blogs)} blogs'
+        # handle notices
+        msgs = messages.get_messages(request)
+        notice = ''
+        for msg in msgs:
+            notice += str(msg)
+        if(notice != ''):
+            message = notice
+
+        return render(request, self.template_name, {'blogs': blogs, 'message': message})
 
     def post(self, request):
         data_str = request.body.decode('utf-8')
@@ -18,18 +29,45 @@ class Blogs(TemplateView):
         description = form_data.get('description')
         blog = Blog(title=title, description=description)
         blog.save()
-        blogs = Blog.objects.all()
-        return render(request, self.template_name, {'blogs': blogs, 'message': 'creating blog'})
+        if(blog.id != None):
+            messages.success(request, 'Blog created successfully')
+        else:
+            messages.success(request, 'Failed creating blog')
+        return redirect('blogs_path')
 
-class SUDBlog(Blogs):
-    def get(self, request, blog_id):
-        return render(request, 'base.html', {'message': 'viewing blog'})
+class RevokeBlog(Blogs):
+    def create_blog(request):
+        blog = Blog()
+        return render(request, 'create.html',{'blog': blog})
 
-    def put(self, request, blog_id):
-        return render(request, 'base.html', {'message': 'updated blog'})
+    def show_blog(request, blog_id):
+        blog = Blog.objects.get(pk=blog_id)
+        return render(request, 'show.html', {'blog': blog})
 
-    def delete(self, request, blog_id):
-        return render(request, 'base.html', {'message': 'deleted blog'})
+    def edit_blog(request, blog_id):
+        blog = Blog.objects.get(pk=blog_id)
+        return render(request, 'edit.html', {'blog': blog})
+
+    def update_blog(request, blog_id):
+        blog = Blog.objects.get(pk=blog_id)
+        data_str = request.body.decode('utf-8')
+        form_data = QueryDict(data_str)
+        blog.title = form_data.get('title')
+        blog.description = form_data.get('description')
+        blog.save()
+        return render(request, 'show.html', {'blog': blog})
+
+    def delete_blog(request, blog_id):
+        try:
+            blog = Blog.objects.get(pk=blog_id)
+            blog.delete()
+            if(blog.id == None):
+                messages.success(request, 'Blog deleted successfully.')
+            else:
+                messages.success(request, 'Blog deletion failed.')
+        except Exception as e:
+            messages.success(request, e)
+        return redirect('blogs_path')
 
 class Comments(TemplateView):
     def index(request, blog_id):
