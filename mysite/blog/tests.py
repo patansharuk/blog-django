2508d33aa01be0a8c03f76ec3df2600385294d10
sharukhan <sharukhan@webkorps.com>
2024-04-30 23:46:29 +0530

    implemented testing views

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class BasicTest(TestCase):
+    def test_should_return_bool(self):
+        res = self.client.get(reverse('blogs_path'))
+        self.assertIs(200, 200)
+
+    def test_login(self):
+        res = self.client.get(reverse('not_found_path'))
+        print(res.content)
+        self.assertIs(True, True)
